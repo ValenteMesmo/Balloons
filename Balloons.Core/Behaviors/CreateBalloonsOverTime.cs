04907dfbea606ec36b483458bb6dc311650960bd
valentemesmo <valentemesmo@gmail.com>
2020-01-07 21:18:20 -0300

    ajusting animation

--- Balloons.Core/Behaviors/CreateBalloonsOverTime.cs
+++ Balloons.Core/Behaviors/CreateBalloonsOverTime.cs
@@ -29,17 +29,19 @@ namespace Balloons.Core
                 var idleAnimation = new List<SpriteData>();
                 var deathAnimation = new List<SpriteData>();
 
+                var cellSize = 170;
+
                 for (int i = 0; i < 5; i++)
                 {
                     idleAnimation.Add(new SpriteData(world.Textures["coin"])
                     {
-                        sourceRectangle = new Rectangle(i* 200, 0, 200, 210),
+                        sourceRectangle = new Rectangle(i* 200, 0, 200, cellSize),
                         targetRectangle = new Rectangle(ballonX, ballonY, GameConstants.BalloonSize, GameConstants.BalloonSize)
                     });
 
                     deathAnimation.Add(new SpriteData(world.Textures["coin"])
                     {
-                        sourceRectangle = new Rectangle(i * 200, 200, 200, 210),
+                        sourceRectangle = new Rectangle(i * 200, cellSize, 200, cellSize),
                         targetRectangle = new Rectangle(ballonX, ballonY, GameConstants.BalloonSize, GameConstants.BalloonSize)
                     });
                 }
